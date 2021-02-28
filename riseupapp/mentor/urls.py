d6b4e42e1717408ec6c9179461576628b9147568
AhmedKayyum_u1735086 <61551433+akayyum@users.noreply.github.com>
2021-02-28 13:45:51 +0000

    File Upload Current

--- riseupapp/mentor/urls.py
+++ riseupapp/mentor/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('guidance/', views.guidance, name='guidance'),
     path('blog/', views.blog, name='blog'),
     path('register/', views.register, name='register'),
+    path('reg/', views.registerPage, name="reg"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
 ]
--- riseupapp/mentor/views.py
+++ riseupapp/mentor/views.py
@@ -1,21 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='login')
 def mentor(request):
 	mentors = Mentor.objects.all()
 	context = {'mentors':mentors}
 	return render(request, 'mentor/mentor.html', context)
 
+@login_required(login_url='login')
 def blog(request):
 	context = {}
 	return render(request, 'mentor/blog.html', context)
 
+@login_required(login_url='login')
 def guidance(request):
 	context = {}
 	return render(request, 'mentor/guidance.html', context)
 
+@login_required(login_url='login')
 def register(request):
 	context = {}
 	return render(request, 'mentor/register.html', context)
+
+
+
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('mentor')
+	else:	
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account created for ' + user)
+
+				return redirect('login')
+
+		context = {'form':form}
+		return render(request, 'mentor/reg.html', context)
+
+
+
+def loginPage(request):
+		if request.user.is_authenticated:
+			return redirect('mentor')
+		else:	
+			if request.method == 'POST':
+				username = request.POST.get('username')
+				password = request.POST.get('password')
+
+				user = authenticate(request, username=username, password=password)
+
+				if user is not None:
+					login(request, user)
+					return redirect('mentor')
+
+				else:
+					messages.info(request, 'username OR password is incorrect')
+
+
+			context = {}
+			return render(request, 'mentor/login.html', context)
+
+
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
