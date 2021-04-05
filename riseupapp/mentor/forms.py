8082aa7e0c62db8b4cfcd408dfbc35100de1cd2a
AhmedKayyum_u1735086 <61551433+akayyum@users.noreply.github.com>
2021-04-05 12:08:39 +0100

    File Upload

--- riseupapp/mentor/forms.py
+++ riseupapp/mentor/forms.py
@@ -2,8 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-# This links in with the details required for user registration.
-
+# This creates a form which takes details required for user registration.
 class CreateUserForm(UserCreationForm):
 	class Meta:
 		model = User
